cb7b526db7278ff3b1a215bbc70413302c3c37f0
Caio Arruda <caioarruda@gmail.com>
2022-12-08 17:01:19 +0000

    fix sghome

--- src/content/pages/smartgate/Home.tsx
+++ src/content/pages/smartgate/Home.tsx
@@ -1,23 +1,23 @@
 import {
-    Box,
-    Card,
-    Typography,
-    Container,
-    Divider,
-    Button,
-    FormControl,
-    TextField,
-    CircularProgress
-  } from '@mui/material';
-  import { Helmet } from 'react-helmet-async';
-  import { styled } from '@mui/material/styles';
-  import { useContext, useEffect, useState } from 'react';
-  import { useLocation, useNavigate } from "react-router-dom";
-  import { AuthContext } from "src/contexts/Auth/AuthContext";
-  import { toast } from 'react-toastify';
-  
-  const MainContent = styled(Box)(
-    ({ theme }) => `
+  Box,
+  Card,
+  Typography,
+  Container,
+  Divider,
+  Button,
+  FormControl,
+  TextField,
+  CircularProgress
+} from '@mui/material';
+import { Helmet } from 'react-helmet-async';
+import { styled } from '@mui/material/styles';
+import { useContext, useEffect, useState } from 'react';
+import { useLocation, useNavigate } from "react-router-dom";
+import { AuthContext } from "src/contexts/Auth/AuthContext";
+import { toast } from 'react-toastify';
+
+const MainContent = styled(Box)(
+  ({ theme }) => `
       height: 100%;
       display: flex;
       flex: 1;
@@ -26,74 +26,62 @@ import {
       align-items: center;
       justify-content: center;
   `
-  );
-  
-  export const SGHome = () => {
-   
-    const gateHandler = (action: String) => {
-        fetch('http://portao.c2atec.com:30147/handler?pass=Ca15ar80@@&action='+action)
-        .then(()=>{
-            toast.success('Portão acionado com sucesso')
-        })
-        .catch(()=>{
-            toast.error('Erro ao acionar o portão')
-        })
-    }
-  
-
-  
-    return (
-      <>
-        <Helmet>
-          <title>Login</title>
-        </Helmet>
-        <MainContent>
-          <Container maxWidth="md">
-            <Container maxWidth="sm">
-              <Card sx={{ textAlign: 'center', mt: 3, p: 4 }}>
-                <Box textAlign="center" >
-                  <img alt="logo" width={200} src="/static/images/logo/c2a.svg" />
-                  <Typography
-                    variant="h4"
-                    color="text.secondary"
-                    fontWeight="normal"
-                    sx={{ my: 2, mb: 4 }}
-                  >
-                    Bem vindo! Para abrir ou fechar o portão clique nos botoes abaixo
-                  </Typography>
-                  <Button
-                    onClick={()=>{gateHandler('open')}}
-                    sx={{ m: 1 }}
-                    variant="contained">
-                    Abrir
-                  </Button>
-                  <Button
-                    
-                    sx={{ m: 1 }}
-                    variant="contained">
-                    Fechar
-                  </Button>
-                  <Button
-                    
-                    sx={{ m: 1 }}
-                    variant="contained">
-                    Abrir e Fechar 
-                  </Button>
-                </Box>
-
-
-                
-                
-  
-  
-                
-              </Card>
-            </Container>
-          </Container>
-        </MainContent>
-  
-      </>
-    );
+);
+export const SGHome = () => {
+  const gateHandler = (action: String) => {
+    fetch('http://portao.c2atec.com:30147/handler?pass=Ca15ar80@@&action=' + action)
+      .then(() => {
+        toast.success('Portão acionado com sucesso')
+      })
+      .catch(() => {
+        toast.error('Erro ao acionar o portão')
+      })
   }
-  
-  export default SGHome;
+  return (
+    <>
+      <Helmet>
+        <title>Login</title>
+      </Helmet>
+      <MainContent>
+        <Container maxWidth="md">
+          <Container maxWidth="sm">
+            <Card sx={{ textAlign: 'center', mt: 3, p: 4 }}>
+              <Box textAlign="center" >
+                <img alt="logo" width={200} src="/static/images/logo/c2a.svg" />
+                <Typography
+                  variant="h4"
+                  color="text.secondary"
+                  fontWeight="normal"
+                  sx={{ my: 2, mb: 4 }}
+                >
+                  Bem vindo! Para abrir ou fechar o portão clique nos botoes abaixo
+                </Typography>
+                <Button
+                  onClick={() => { gateHandler('open') }}
+                  sx={{ m: 1 }}
+                  variant="contained">
+                  Abrir
+                </Button>
+                <Button
+
+                  sx={{ m: 1 }}
+                  variant="contained">
+                  Fechar
+                </Button>
+                <Button
+
+                  sx={{ m: 1 }}
+                  variant="contained">
+                  Abrir e Fechar
+                </Button>
+              </Box>
+            </Card>
+          </Container>
+        </Container>
+      </MainContent>
+
+    </>
+  );
+}
+
+export default SGHome;
